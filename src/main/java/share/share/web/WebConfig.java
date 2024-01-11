1c43a7c8763fc8ce1cf950b922550d02fbe5dca5
jakobmertenhtw <147500519+jakobmertenhtw@users.noreply.github.com>
2024-01-11 15:30:16 +0100

    fix bug cors policy

--- src/main/java/share/share/web/WebConfig.java
+++ src/main/java/share/share/web/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://127.0.0.1:5173");
+                .allowedOrigins("http://127.0.0.1:5173")
+                .allowedOrigins("http://localhost:5173");
     }
 }
